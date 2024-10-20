396849ebebf1246793e68e097aff7ad0b4934fa5
unknown <keerthana.siripuram1201@gmail.com>
2024-10-20 20:55:31 +0530

    changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -57,7 +57,8 @@ module.exports.login = async (req, res) => {
         }
     }
     catch (error) {
-        return res.status(500).send({ message: "Error logging user" + error.mesaage, success: false, error })
+        console.log(error)
+        return res.status(500).send({ message: "Error logging user" + , success: false, error })
     }
 }
 
